4f2cf2fa91c8097b97774324ff1a88bb9be58a51
Czapa10 <Gesio888lol@wp.pl>
2018-12-30 15:10:39 +0100

    make mouseInput methods shorter (REFACTOR)

--- src/mouseInput.cpp
+++ src/mouseInput.cpp
@@ -9,9 +9,7 @@ bool MouseInput::isClicked(sf::Sprite sprite, sf::RenderWindow &window)
     if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
         sf::IntRect button(sprite.getPosition().x, sprite.getPosition().y, sprite.getGlobalBounds().width, sprite.getGlobalBounds().height);
 
-        if(button.contains(sf::Mouse::getPosition(window))){
-            return true;
-        }
+        return (button.contains(sf::Mouse::getPosition(window)));
     }
     return false;
 }
@@ -19,9 +17,7 @@ bool MouseInput::isClicked(sf::Sprite sprite, sf::RenderWindow &window)
 bool MouseInput::isClicked(sf::IntRect button, sf::RenderWindow &window)
 {
     if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
-        if(button.contains(sf::Mouse::getPosition(window))){
-            return true;
-        }
+        return (button.contains(sf::Mouse::getPosition(window)));
     }
     return false;
 }
@@ -30,18 +26,12 @@ bool MouseInput::isUnderMouse(sf::Sprite sprite, sf::RenderWindow &window)
 {
     sf::IntRect button(sprite.getPosition().x, sprite.getPosition().y, sprite.getGlobalBounds().width, sprite.getGlobalBounds().height);
 
-    if(button.contains(sf::Mouse::getPosition(window))){
-        return true;
-    }
-    return false;
+    return (button.contains(sf::Mouse::getPosition(window)));
 }
 
 bool MouseInput::isUnderMouse(sf::IntRect button, sf::RenderWindow &window)
 {
-    if(button.contains(sf::Mouse::getPosition(window))){
-        return true;
-    }
-    return false;
+    return (button.contains(sf::Mouse::getPosition(window)));
 }
 
 
