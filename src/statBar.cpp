689d31ef6973634bf5863671ebc7f54034cfc010
Czapa10 <Gesio888lol@wp.pl>
2019-01-15 00:29:09 +0100

    statistic bar on the middle

--- src/statBar.cpp
+++ src/statBar.cpp
@@ -1,16 +1,18 @@
 #include "statBar.hpp"
 
+#define BAR_X 259
+
 namespace GameElements
 {
 
 
 StatBar::StatBar(Game::GameDataRef _data) : data(_data)
-,pointsDisplayer(data, 92, 0)
-,snakeLengthDisplayer(data, 325, 0)
-,inStomachDisplayer(data, 515, 0)
+,pointsDisplayer(data, 92 + BAR_X, 0)
+,snakeLengthDisplayer(data, 325 + BAR_X, 0)
+,inStomachDisplayer(data, 515 + BAR_X, 0)
 {
     barBackground.setTexture(data->textures.get(Textures::statisticsBar));
-    barBackground.setPosition(0, 5);
+    barBackground.setPosition(BAR_X, 5);
 }
 
 void StatBar::draw(int points, int snakeLength, int inStomach) const
