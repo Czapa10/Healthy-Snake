569b4d749cdc9538c743a68d9259fff61e3bc03c
Czapa10 <Gesio888lol@wp.pl>
2019-03-07 13:29:05 +0100

    SoundPlayer implementation

--- src/Audio/Sound/soundPlayer.cpp
+++ src/Audio/Sound/soundPlayer.cpp
@@ -6,17 +6,26 @@ namespace Audio
 
 SoundPlayer::SoundPlayer()
 {
-
+    soundBufferStorage.load(Sounds::cat, "resources/sounds/cat.wav");
+    soundBufferStorage.load(Sounds::eat, "resources/sounds/eat.wav");
+    soundBufferStorage.load(Sounds::die, "resources/sounds/die.wav");
+    soundBufferStorage.load(Sounds::explosion, "resources/sounds/explosion.wav");
 }
 
-void SoundPlayer::play(Sounds::ID)
+void SoundPlayer::play(Sounds::ID soundID)
 {
+    removeStoppedSounds();
 
+    sounds.push_back(sf::Sound(soundBufferStorage.get(soundID)));
+    sounds.back().play();
 }
 
 void SoundPlayer::removeStoppedSounds()
 {
-
+    sounds.remove_if( [] (const sf::Sound& s)
+    {
+        return s.getStatus() == sf::Sound::Stopped;
+    } );
 }
 
 
