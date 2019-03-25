64e29474a0e0f578fe95090bc648a5efc54673ff
Czapa10 <Gesio888lol@wp.pl>
2019-03-25 16:39:52 +0100

    BestScoreManager header was created

--- src/Save/BestScoresManager/bestScoresManager.hpp
+++ src/Save/BestScoresManager/bestScoresManager.hpp
@@ -1,12 +1,27 @@
 #pragma once
 
+#include <States/DifficultyChoise/difficultyLevelIdentifiers.hpp>
+
 namespace Save
 {
 
 
 class BestScoresManager
 {
+public:
+    BestScoresManager();
 
+    void transferScore(int score);
+    int getScore(Difficulty::Level) const;
+
+private:
+    void saveScore();
+    void loadScore();
+
+private:
+    int easyBestScore;
+    int mediumBestScore;
+    int hardBestScore;
 };
 
 
